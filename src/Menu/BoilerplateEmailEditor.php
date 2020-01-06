66c4f09eaf352f9bfe6d2df210c9e675a69176c5
Sebastien HEYD <sebastien.heyd@free.fr>
2020-01-06 15:09:25 +0100

    Fix StyleCI

--- src/Menu/BoilerplateEmailEditor.php
+++ src/Menu/BoilerplateEmailEditor.php
@@ -9,9 +9,9 @@ class BoilerplateEmailEditor
     public function make(Builder $menu)
     {
         $menu->add(__('boilerplate-email-editor::editor.title'), [
-                'permission' => 'emaileditor_email_edition,emaileditor_email_dev',
-                'route'      => 'emaileditor.email.index',
-                'icon'       => 'mail-bulk', ])
+            'permission' => 'emaileditor_email_edition,emaileditor_email_dev',
+            'route'      => 'emaileditor.email.index',
+            'icon'       => 'mail-bulk', ])
             ->id('emaileditor')
             ->activeIfRoute('emaileditor.*')
             ->order(900);
