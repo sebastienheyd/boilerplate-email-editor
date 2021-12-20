daf173aba80d78fd24e8c09e7573e1e730b9a3d9
Sébastien HEYD <sebastien.heyd@free.fr>
2021-12-20 17:17:12 +0100

    Menu items

--- src/Menu/BoilerplateEmailEditor.php
+++ src/Menu/BoilerplateEmailEditor.php
@@ -10,10 +10,20 @@ class BoilerplateEmailEditor
     {
         $menu->add(__('boilerplate-email-editor::editor.title'), [
             'permission' => 'emaileditor_email_edition,emaileditor_email_dev',
+            'icon'       => 'mail-bulk',
+            'active'     => 'emaileditor.*',
+        ])->id('emaileditor')->order(900);
+
+        $menu->addTo('emaileditor', __('boilerplate-email-editor::email.list'), [
+            'permission' => 'emaileditor_email_edition,emaileditor_email_dev',
             'route'      => 'emaileditor.email.index',
-            'icon'       => 'mail-bulk', ])
-            ->id('emaileditor')
-            ->activeIfRoute('emaileditor.*')
-            ->order(900);
+            'active'     => 'emaileditor.email.index',
+        ]);
+
+        $menu->addTo('emaileditor', __('boilerplate-email-editor::email.add'), [
+            'permission' => 'emaileditor_email_edition,emaileditor_email_dev',
+            'route'      => 'emaileditor.email.create',
+            'active'     => 'emaileditor.email.create',
+        ]);
     }
 }
